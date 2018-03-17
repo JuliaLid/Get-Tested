d3dd37c2184259b7ecbc25cb67fcc7d78cc99e16
Nick Holmin <niholm@jedunn.com>
2018-03-17 08:45:19 -0500

    working version of test model

--- models/stdtest.js
+++ models/stdtest.js
@@ -1,5 +1,30 @@
 module.exports = function(sequelize, DataTypes) {
-    var StdTest
+    var StdTest = sequelize.define("StdTest", {
+    	test_name: {
+    		type: Sequelize.ENUM,
+    		values: ['Throat Swab', 'RectalSwab', 'UrethralSwab', 'UrineSample', 'PAPSmear', 'AnalPAP', 'BloodDraw', 'FingerStick', 'VisualExam']
+    	},
+    	multiple_partners: {
+    		type: Sequelize.BOOLEAN
+    	},
+    	sex_act: {
+    		type: Sequelize.ENUM,
+    		values: ['Oral', 'Anal', 'Vaginal']
+    	},
+    	symptoms: {
+    		type: Sequelize.BOOLEAN
+    	},
+    	Gonorrhea: {
+    		type: Sequelize.BOOLEAN
+    	},
+    	Chlamydia: {
+    		type: Sequelize.BOOLEAN
+    	},
+    	HIV: {
+    		type: Sequelize.BOOLEAN
+    	},
+
+    });
     
     //sequelize model for the stdtest database goes here
     //current db name is stdtest_db
