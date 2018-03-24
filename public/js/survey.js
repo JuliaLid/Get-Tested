2dced19642262675ee61e8674622782dc6977b4e
Julia Lidstrom <juleslidstrom@gmail.com>
2018-03-24 10:37:36 -0500

    Working test results

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,14 +1,46 @@
 $(document).ready(function(){
 
-    //add all AJAX calls to capture user's survey info
-    //if we add other pages with inputs, we'll need to set up another file 
+    
 
     jQuery.noConflict();
 
-       //accordion control
-    
-    //survey.js fucntions
-    // Survey.StylesManager.applyTheme("default");
+    var data = {
+        // A labels array that can contain any sort of values
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+        // Our series array that contains series objects or in this case series data arrays
+        series: [
+            [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+            [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+          ]
+      };
+
+      var options = {
+        width: 600,
+        height: 200,
+        seriesBarDistance: 15
+      };
+
+      var responsiveOptions = [
+        ['screen and (min-width: 641px) and (max-width: 1024px)', {
+          seriesBarDistance: 10,
+          axisX: {
+            labelInterpolationFnc: function (value) {
+              return value;
+            }
+          }
+        }],
+        ['screen and (max-width: 640px)', {
+          seriesBarDistance: 5,
+          axisX: {
+            labelInterpolationFnc: function (value) {
+              return value[0];
+            }
+          }
+        }]
+      ];
+      
+      new Chartist.Bar('.ct-chart', data, options, responsiveOptions);
+      
 
     Survey.Survey.cssType = "bootstrap";
     
