b4d0b11c00c6849b066c998d2fab383acc761119
David Lopez <dwlopez91@gmail.com>
2018-03-26 20:18:54 -0500

    nightmare test works

--- test/test.js
+++ test/test.js
@@ -1,73 +1,22 @@
-var expect = require("chai").expect;
-var maleResults = require("./functions");
-//var maleResults = maleResults;
-//var femaleResults = results.femaleResults;
+var Nightmare = require("nightmare");
 
-describe("Test type", function() {
-    it("Match STD test with sex acts", function() {
-        var testArray = ["test1, test2, test3, test4, test5, test6"];
-        expect(testArray).to.have.members(["test1"]);
-    })
-})
+var nightmare = Nightmare({ show: true });
+var url ='https://get-tested.herokuapp.com'
+nightmare
 
-// describe("Anal sex male/female", function() {
-//     it("takes a sex act and returns appropriate test priorities", function() {
-//         expect(resultsMale("Anal sex with a woman")).to.equal("UrineSample, FingerStick");
-//     })
-// });
+  .goto(url)
+  .wait(3000)
+  .click("#download-button")
+  .wait("#links a")
+  .evaluate(function() {
+    return document.querySelector("#links a").href;
+  })
+  .end()
+  .then(function(result) {
+    console.log(result);
+  })
+  .catch(function(error) {
+    console.error("Search failed:", error);
 
-// describe("Oral sex to male", function() {
-//     it("takes a sex act and returns appropriate test priorities", function() {
-//         expect(resultsMale("Giving oral sex to a man")).to.equal("ThroatSwab");
-//     })
-// });
-
-// describe("Topping", function() {
-//     it("takes a sex act and returns appropriate test priorities", function() {
-//         expect(resultsMale("Anal sex with man (top)")).to.equal("FingerStick, UrineSample");
-//     })
-// });
-
-
-// describe("Bottoming", function() {
-//     it("takes a sex act and returns appropriate test priorities", function() {
-//         expect(resultsMale("Anal sex with man (bottom)")).to.equal("FingerStick, RectalSwab");
-//     })
-// });
-
-// describe("Receive oral sex", function() {
-//     it("takes a sex act and returns appropriate test priorities", function() {
-//         expect(resultsMale("Receiving oral sex")).to.equal("UrineSample");
-//     })
-// });
-
-// //----------------------------------------------------------------------------------------
-// //Tests for resultsFemale 
-
-// describe("Vaginal sex Female", function() {
-//     it("takes a sex act and returns appropriate test priorities", function() {
-//         expect(resultsFemale("Vaginal")).to.equal("UrineSample, FingerStick");
-//     })
-// });
-
-// describe("Oral sex give", function() {
-//     it("takes a sex act and returns appropriate test priorities", function() {
-//         expect(resultsFemale("Giving oral sex to a man")).to.equal("ThroatSwab");
-//     })
-// });
-
-// describe("Anal sex female/male", function() {
-//     it("takes a sex act and returns appropriate test priorities", function() {
-//         expect(resultsFemale("Anal")).to.equal("RectalSwab, FingerStick");
-//     })
-// });
-
-// describe("receive oral sex", function() {
-//     it("takes a sex act and returns appropriate test priorities", function() {
-//         expect(resultsFemale("Receiving oral sex")).to.equal("UrineSample");
-//     })
-// });
-
-
-
+});
 
